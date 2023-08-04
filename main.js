3e138cbd7d169f82425f415e6252f3d25c5402be
Hiep Nguyen <122915998+Hiep-Nguyen1@users.noreply.github.com>
2023-08-04 10:51:27 -0500

    Add operator choice function

--- main.js
+++ main.js
@@ -18,15 +18,35 @@ function add (calcInfo){
 }
 
 function subtract (calcInfo){
-    return calcInfo.currentNum + calcInfo.secondNum;
+    return calcInfo.currentNum - calcInfo.secondNum;
 }
 
 function divide (calcInfo){
-    return calcInfo.currentNum / calcInfo.secondNum;
+    return (calcInfo.currentNum / calcInfo.secondNum).toFixed(3);
 }
 
 function multiply (calcInfo){
     return calcInfo.currentNum * calcInfo.secondNum;
+}
+
+function operator(calcInfo){
+    switch (calcInfo.operator){
+        case 'add':
+            return add(calcInfo);
+            break;
+        case 'subtract':
+            return subtract(calcInfo);
+            break;
+        case 'divide':
+            return divide(calcInfo);
+            break;
+        case 'multiply':
+            return multiply(calcInfo);
+            break;
+        case 'percent':
+
+            break;
+    }
 }
 
 createButtons();
